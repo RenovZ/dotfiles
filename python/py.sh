dbc4bef986fcfba27be3831a100811131b604a22
cgxxv <cghack@yeah.net>
2022-04-07 00:17:19 +0800

    Fuck python

--- python/py.sh
+++ python/py.sh
@@ -1,25 +1,17 @@
 #!/usr/bin/env bash
 
-# pkgs=(
-#   autojump bind fpp gdb git-filter-repo grc httpie meson mpv percol pipx rtv thefuck trash-cli yt-dlp
+# pkgs=`cat pkgs.txt`
 
-#   adwaita-icon-theme cairo cairomm cairomm@1.14 desktop-file-utils ffmpeg@4 ffmpegthumbnailer fontforge gdk-pixbuf glib glibmm gobject-introspection graphviz gsettings-desktop-schemas gts harfbuzz itstool libass libcroco liblqr libnotify librsvg mercurial mitmproxy mycli pango pgcli py2cairo py3cairo pygobject3 python-tabulate tbb vapoursynth
+# for pkg in ${pkgs[@]}; do
+#   echo $pkg
 
-#   ffmpeg@4  fontforge  gobject-introspection  graphviz  harfbuzz  libass  librsvg  pango  py2cairo  py3cairo pygobject3
+#   #brew uninstall --ignore-dependencies ${pkg}
+#   #pip install -U ${pkg}
+# done
 
-#   graphviz  harfbuzz  libass  librsvg  pango pygobject3
-
-#   libass librsvg pango
-
-#   pynvim
-
-# )
-
-pkgs=`cat pkgs.txt`
-
-for pkg in ${pkgs[@]}; do
+cat pkgs | while read pkg; do
   echo $pkg
 
-  brew uninstall --ignore-dependencies ${pkg}
+  echo $pkg|awk '{print $1}'|xargs -I {} brew uninstall --ignore-dependencies {}
   pip install -U ${pkg}
 done
