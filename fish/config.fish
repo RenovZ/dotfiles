8897703ba26ede36960ae3e241673d99e420efdb
cgxxv <cghack@yeah.net>
2022-05-10 15:38:15 +0800

    Speed up fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,17 @@
 bass source ~/.config/sys-config/profile
 
-set --erase NNN_TRASH
+luarocks path --bin | source
+thefuck --alias | source
 
 zoxide init fish | source
 mcfly init fish | source
 
+# pyenv (asdf still uses pyenv under the hood)
+set -gx PYTHON_BUILD_ARIA2_OPTS "-x 10 -k 1M" # Use aria2c when downloading
+source ~/.asdf/asdf.fish
+
 set -U fish_user_paths $fish_user_paths "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/"
 set -Ux VISUAL vim
-
-function nvm
-    bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
-end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
