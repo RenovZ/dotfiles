832a82e5984f7702dc86bcd2cab9da9d0050e004
cgxxv <cghack@yeah.net>
2022-05-19 23:03:36 +0800

    Boringly migrate from zsh to fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,16 @@
 bass source ~/.config/sys-config/profile
 
-luarocks path --bin | source
-thefuck --alias | source
-
-zoxide init fish | source
-mcfly init fish | source
-
 # pyenv (asdf still uses pyenv under the hood)
 set -gx PYTHON_BUILD_ARIA2_OPTS "-x 10 -k 1M" # Use aria2c when downloading
 source ~/.asdf/asdf.fish
 
-set -U fish_user_paths $fish_user_paths "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/"
+thefuck --alias | source
+
+luarocks path --bin | source
+
+zoxide init fish | source
+mcfly init fish | source
+
 set -Ux VISUAL vim
 
 if status is-interactive
