c2bb72a38c98909ae4be3609ba104f6e3f40420c
cgxxv <cgsixv@gmail.com>
2023-01-28 11:41:08 +0800

    Add warp config

--- profile.fish
+++ profile.fish
@@ -1,7 +1,65 @@
-# adding TINYGOROOT=/Users/zzq/.tinygo
-set -g -x TINYGOROOT "/Users/zzq/.tinygo"
-# updating PATH=/Users/zzq/.wasmer/bin:/Volumes/Store/.asdf/installs/emsdk/3.1.30:/Volumes/Store/.asdf/installs/emsdk/3.1.30/upstream/emscripten:/Volumes/Store/.asdf/installs/emsdk/3.1.30/node/14.18.2_64bit/bin:/Users/zzq/bin:/opt/local/bin:/usr/local/bin:/opt/local/sbin:/usr/local/sbin:/Users/zzq/.pnpm:/Users/zzq/.deno/bin:/Users/zzq/.basher/bin:/Users/zzq/.sdkbox/bin:/usr/local/opt/make/libexec/gnubin:/Volumes/SOFT/Android/sdk/platform-tools:/Users/zzq/.arkade/bin:/Users/zzq/.golang/bin:/Users/zzq/.asdf/installs/python/3.10.5/bin:/Users/zzq/.asdf/shims:/Users/zzq/.asdf/bin:/Applications/Visual Studio Code.app/Contents/Resources/app/bin/:/Users/zzq/.wasmer/bin:/opt/local/share/luarocks/bin:/Users/zzq/.asdf/installs/lua/5.1.5/bin:/Users/zzq/.asdf/installs/lua/5.1.5/luarocks/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/Apple/usr/bin:/Users/zzq/.luarocks/bin:/Users/zzq/.cargo/bin:/Applications/kitty.app/Contents/MacOS:/Users/zzq/.wasmer/globals/wapm_packages/.bin:/Users/zzq/.wasmer/globals/wapm_packages/.bin -> /Users/zzq/.wasmer/bin:/Volumes/Store/.asdf/installs/emsdk/3.1.30:/Volumes/Store/.asdf/installs/emsdk/3.1.30/upstream/emscripten:/Volumes/Store/.asdf/installs/emsdk/3.1.30/node/14.18.2_64bit/bin:/Users/zzq/.tinygo/bin:/Users/zzq/bin:/opt/local/bin:/usr/local/bin:/opt/local/sbin:/usr/local/sbin:/Users/zzq/.pnpm:/Users/zzq/.deno/bin:/Users/zzq/.basher/bin:/Users/zzq/.sdkbox/bin:/usr/local/opt/make/libexec/gnubin:/Volumes/SOFT/Android/sdk/platform-tools:/Users/zzq/.arkade/bin:/Users/zzq/.golang/bin:/Users/zzq/.wasmer/bin:/Users/zzq/.asdf/installs/python/3.10.5/bin:/Users/zzq/.asdf/shims:/Users/zzq/.asdf/bin:/Applications/Visual Studio Code.app/Contents/Resources/app/bin/:/opt/local/share/luarocks/bin:/Users/zzq/.asdf/installs/lua/5.1.5/bin:/Users/zzq/.asdf/installs/lua/5.1.5/luarocks/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/Apple/usr/bin:/Users/zzq/.luarocks/bin:/Users/zzq/.cargo/bin:/Applications/kitty.app/Contents/MacOS:/Users/zzq/.wasmer/globals/wapm_packages/.bin:/Users/zzq/.wasmer/globals/wapm_packages/.bin
-set -g -x PATH "/Users/zzq/.wasmer/bin" "/Volumes/Store/.asdf/installs/emsdk/3.1.30" "/Volumes/Store/.asdf/installs/emsdk/3.1.30/upstream/emscripten" "/Volumes/Store/.asdf/installs/emsdk/3.1.30/node/14.18.2_64bit/bin" "/Users/zzq/.tinygo/bin" "/Users/zzq/bin" "/opt/local/bin" "/usr/local/bin" "/opt/local/sbin" "/usr/local/sbin" "/Users/zzq/.pnpm" "/Users/zzq/.deno/bin" "/Users/zzq/.basher/bin" "/Users/zzq/.sdkbox/bin" "/usr/local/opt/make/libexec/gnubin" "/Volumes/SOFT/Android/sdk/platform-tools" "/Users/zzq/.arkade/bin" "/Users/zzq/.golang/bin" "/Users/zzq/.wasmer/bin" "/Users/zzq/.asdf/installs/python/3.10.5/bin" "/Users/zzq/.asdf/shims" "/Users/zzq/.asdf/bin" "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/" "/opt/local/share/luarocks/bin" "/Users/zzq/.asdf/installs/lua/5.1.5/bin" "/Users/zzq/.asdf/installs/lua/5.1.5/luarocks/bin" "/System/Cryptexes/App/usr/bin" "/usr/bin" "/bin" "/usr/sbin" "/sbin" "/opt/X11/bin" "/Library/Apple/usr/bin" "/Users/zzq/.luarocks/bin" "/Users/zzq/.cargo/bin" "/Applications/kitty.app/Contents/MacOS" "/Users/zzq/.wasmer/globals/wapm_packages/.bin" "/Users/zzq/.wasmer/globals/wapm_packages/.bin"
+# adding ARKADE_PATH=/Users/zzq/.arkade
+set -g -x ARKADE_PATH "/Users/zzq/.arkade"
+# adding NNN_SEL=/tmp/.sel
+set -g -x NNN_SEL "/tmp/.sel"
+# adding GOPRIVATE=bundle.lo
+set -g -x GOPRIVATE "bundle.lo"
+# adding NNN_FIFO=/tmp/nnn.fifo
+set -g -x NNN_FIFO "/tmp/nnn.fifo"
+# adding GOROOT_BOOTSTRAP=/usr/local/go
+set -g -x GOROOT_BOOTSTRAP "/usr/local/go"
+# adding EDITOR=nvim
+set -g -x EDITOR "nvim"
+# adding GNU_HOME=/usr/local/opt/make/libexec
+set -g -x GNU_HOME "/usr/local/opt/make/libexec"
+# adding EMSDK_PYTHON=/Volumes/Store/.asdf/installs/emsdk/3.1.30/python/3.9.2_64bit/bin/python3
+set -g -x EMSDK_PYTHON "/Volumes/Store/.asdf/installs/emsdk/3.1.30/python/3.9.2_64bit/bin/python3"
+# adding VCPKG_ROOT=/Users/zzq/vcpkg
+set -g -x VCPKG_ROOT "/Users/zzq/vcpkg"
+# adding PNPM_HOME=/Users/zzq/.pnpm
+set -g -x PNPM_HOME "/Users/zzq/.pnpm"
+# adding FZF_DEFAULT_COMMAND=fd --type f -H -E ".git/"
+set -g -x FZF_DEFAULT_COMMAND "fd --type f -H -E \".git/\""
+# adding NNN_PLUG=v:imgview;f:fzcd;t:preview-tui;T:preview-tabbed;p:-!less -iR $nnn*
+set -g -x NNN_PLUG "v:imgview;f:fzcd;t:preview-tui;T:preview-tabbed;p:-!less -iR \$nnn*"
+# adding DENO_HOME=/Users/zzq/.deno
+set -g -x DENO_HOME "/Users/zzq/.deno"
+# adding BAT_THEME=Solarized (dark)
+set -g -x BAT_THEME "Solarized (dark)"
+# adding LE_WORKING_DIR=/Users/zzq/.acme.sh
+set -g -x LE_WORKING_DIR "/Users/zzq/.acme.sh"
+# adding GOENV=/Users/zzq/.golang/env
+set -g -x GOENV "/Users/zzq/.golang/env"
+# adding GOPROXY=https://goproxy.cn,direct
+set -g -x GOPROXY "https://goproxy.cn,direct"
+# adding SDKBOX_HOME=/Users/zzq/.sdkbox
+set -g -x SDKBOX_HOME "/Users/zzq/.sdkbox"
+# adding EMSDK_NODE=/Volumes/Store/.asdf/installs/emsdk/3.1.30/node/14.18.2_64bit/bin/node
+set -g -x EMSDK_NODE "/Volumes/Store/.asdf/installs/emsdk/3.1.30/node/14.18.2_64bit/bin/node"
+# adding GOCACHE=/Users/zzq/.golang/go-build
+set -g -x GOCACHE "/Users/zzq/.golang/go-build"
+# adding NNN_HELP=pwy paris
+set -g -x NNN_HELP "pwy paris"
+# adding SSL_CERT_FILE=/Volumes/Store/.asdf/installs/emsdk/3.1.30/python/3.9.2_64bit/lib/python3.9/site-packages/certifi/cacert.pem
+set -g -x SSL_CERT_FILE "/Volumes/Store/.asdf/installs/emsdk/3.1.30/python/3.9.2_64bit/lib/python3.9/site-packages/certifi/cacert.pem"
+# adding GO111MODULE=on
+set -g -x GO111MODULE "on"
+# adding LC_ALL=en_US.UTF-8
+set -g -x LC_ALL "en_US.UTF-8"
+# adding LUA_CPATH=/opt/local/lib/lua/5.3/?.so;/opt/local/lib/lua/5.3/loadall.so;./?.so;/Users/zzq/.luarocks/lib/lua/5.3/?.so;/opt/local/share/luarocks/lib/lua/5.3/?.so
+set -g -x LUA_CPATH "/opt/local/lib/lua/5.3/?.so;/opt/local/lib/lua/5.3/loadall.so;./?.so;/Users/zzq/.luarocks/lib/lua/5.3/?.so;/opt/local/share/luarocks/lib/lua/5.3/?.so"
+# updating PATH=/Users/zzq/.asdf/installs/python/3.10.5/bin:/Users/zzq/.asdf/shims:/Users/zzq/.asdf/bin:/Applications/Visual Studio Code.app/Contents/Resources/app/bin/:/Users/zzq/.wasmer/bin:/Volumes/Store/.asdf/installs/emsdk/3.1.30:/Volumes/Store/.asdf/installs/emsdk/3.1.30/upstream/emscripten:/Volumes/Store/.asdf/installs/emsdk/3.1.30/node/14.18.2_64bit/bin:/Users/zzq/.tinygo/bin:/Users/zzq/bin:/opt/local/bin:/usr/local/bin:/opt/local/sbin:/usr/local/sbin:/Users/zzq/.pnpm:/Users/zzq/.deno/bin:/Users/zzq/.basher/bin:/Users/zzq/.sdkbox/bin:/usr/local/opt/make/libexec/gnubin:/Volumes/SOFT/Android/sdk/platform-tools:/Users/zzq/.arkade/bin:/Users/zzq/.golang/bin:/Users/zzq/.wasmer/bin:/Users/zzq/.asdf/installs/python/3.10.5/bin:/opt/local/share/luarocks/bin:/Users/zzq/.asdf/installs/lua/5.1.5/bin:/Users/zzq/.asdf/installs/lua/5.1.5/luarocks/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/Apple/usr/bin:/Users/zzq/.luarocks/bin:/Users/zzq/.cargo/bin:/Applications/kitty.app/Contents/MacOS:/Users/zzq/.wasmer/globals/wapm_packages/.bin:/Users/zzq/.wasmer/globals/wapm_packages/.bin -> /Users/zzq/.wasmer/bin:/Volumes/Store/.asdf/installs/emsdk/3.1.30:/Volumes/Store/.asdf/installs/emsdk/3.1.30/upstream/emscripten:/Volumes/Store/.asdf/installs/emsdk/3.1.30/node/14.18.2_64bit/bin:/Users/zzq/.tinygo/bin:/Users/zzq/bin:/opt/local/bin:/usr/local/bin:/opt/local/sbin:/usr/local/sbin:/Users/zzq/.pnpm:/Users/zzq/.deno/bin:/Users/zzq/.basher/bin:/Users/zzq/.sdkbox/bin:/usr/local/opt/make/libexec/gnubin:/Volumes/SOFT/Android/sdk/platform-tools:/Users/zzq/.arkade/bin:/Users/zzq/.golang/bin:/Users/zzq/.asdf/installs/python/3.10.5/bin:/Users/zzq/.asdf/shims:/Users/zzq/.asdf/bin:/Applications/Visual Studio Code.app/Contents/Resources/app/bin/:/Users/zzq/.wasmer/bin:/opt/local/share/luarocks/bin:/Users/zzq/.asdf/installs/lua/5.1.5/bin:/Users/zzq/.asdf/installs/lua/5.1.5/luarocks/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/Apple/usr/bin:/Users/zzq/.luarocks/bin:/Users/zzq/.cargo/bin:/Applications/kitty.app/Contents/MacOS:/Users/zzq/.wasmer/globals/wapm_packages/.bin:/Users/zzq/.wasmer/globals/wapm_packages/.bin
+set -g -x PATH "/Users/zzq/.wasmer/bin" "/Volumes/Store/.asdf/installs/emsdk/3.1.30" "/Volumes/Store/.asdf/installs/emsdk/3.1.30/upstream/emscripten" "/Volumes/Store/.asdf/installs/emsdk/3.1.30/node/14.18.2_64bit/bin" "/Users/zzq/.tinygo/bin" "/Users/zzq/bin" "/opt/local/bin" "/usr/local/bin" "/opt/local/sbin" "/usr/local/sbin" "/Users/zzq/.pnpm" "/Users/zzq/.deno/bin" "/Users/zzq/.basher/bin" "/Users/zzq/.sdkbox/bin" "/usr/local/opt/make/libexec/gnubin" "/Volumes/SOFT/Android/sdk/platform-tools" "/Users/zzq/.arkade/bin" "/Users/zzq/.golang/bin" "/Users/zzq/.asdf/installs/python/3.10.5/bin" "/Users/zzq/.asdf/shims" "/Users/zzq/.asdf/bin" "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/" "/Users/zzq/.wasmer/bin" "/opt/local/share/luarocks/bin" "/Users/zzq/.asdf/installs/lua/5.1.5/bin" "/Users/zzq/.asdf/installs/lua/5.1.5/luarocks/bin" "/System/Cryptexes/App/usr/bin" "/usr/bin" "/bin" "/usr/sbin" "/sbin" "/opt/X11/bin" "/Library/Apple/usr/bin" "/Users/zzq/.luarocks/bin" "/Users/zzq/.cargo/bin" "/Applications/kitty.app/Contents/MacOS" "/Users/zzq/.wasmer/globals/wapm_packages/.bin" "/Users/zzq/.wasmer/globals/wapm_packages/.bin"
+# adding EMSDK=/Volumes/Store/.asdf/installs/emsdk/3.1.30
+set -g -x EMSDK "/Volumes/Store/.asdf/installs/emsdk/3.1.30"
+# adding WASMER_CACHE_DIR=/Users/zzq/.wasmer/cache
+set -g -x WASMER_CACHE_DIR "/Users/zzq/.wasmer/cache"
+# adding LUA_PATH=/opt/local/share/lua/5.3/?.lua;/opt/local/share/lua/5.3/?/init.lua;/opt/local/lib/lua/5.3/?.lua;/opt/local/lib/lua/5.3/?/init.lua;./?.lua;./?/init.lua;/Users/zzq/.luarocks/share/lua/5.3/?.lua;/Users/zzq/.luarocks/share/lua/5.3/?/init.lua;/opt/local/share/luarocks/share/lua/5.3/?.lua;/opt/local/share/luarocks/share/lua/5.3/?/init.lua
+set -g -x LUA_PATH "/opt/local/share/lua/5.3/?.lua;/opt/local/share/lua/5.3/?/init.lua;/opt/local/lib/lua/5.3/?.lua;/opt/local/lib/lua/5.3/?/init.lua;./?.lua;./?/init.lua;/Users/zzq/.luarocks/share/lua/5.3/?.lua;/Users/zzq/.luarocks/share/lua/5.3/?/init.lua;/opt/local/share/luarocks/share/lua/5.3/?.lua;/opt/local/share/luarocks/share/lua/5.3/?/init.lua"
+# adding WASMER_DIR=/Users/zzq/.wasmer
+set -g -x WASMER_DIR "/Users/zzq/.wasmer"
+# adding GOPATH=/Users/zzq/.golang
+set -g -x GOPATH "/Users/zzq/.golang"
 alias "acme.sh"='/Users/zzq/.acme.sh/acme.sh'
 alias "emacs"='TERM=xterm-24bits emacs'
 alias "ll"='ls -agl'
