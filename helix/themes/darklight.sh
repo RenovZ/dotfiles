27fa426252423c6176665fe8553e9ac159ff772c
cgxxv <cgsixv@gmail.com>
2024-07-01 12:19:35 +0800

    Update helix config

--- helix/themes/darklight.sh
+++ helix/themes/darklight.sh
@@ -2,6 +2,8 @@
 
 # https://github.com/qdentity/darklight
 
+export PATH=${HOME}/.cargo/bin:$PATH
+
 if [[ "$(darklight)" == *"Dark"* ]]; then
 	mode="dark"
 else
@@ -11,4 +13,17 @@ fi
 THEME_TARGET="${HOME}/.config/helix/themes/${mode}.toml"
 THEME_LINK="${HOME}/.config/helix/themes/adaptive.toml"
 
-ln -sf $THEME_TARGET $THEME_LINK
+target_file=$(readlink -f $THEME_LINK)
+file_name=$(basename $target_file)
+file_name_no_ext="${file_name%.*}"
+
+if [[ "${file_name_no_ext}" != *"${mode}"* ]]; then
+	ln -sf $THEME_TARGET $THEME_LINK
+fi
+
+# echo "mode:" $mode
+# echo "target_file:" $target_file
+# echo "file_name:" $file_name
+# echo "file_name_no_ext:" $file_name_no_ext
+#
+# ln -sf $THEME_TARGET $THEME_LINK
