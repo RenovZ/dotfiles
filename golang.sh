db2040e024d44afa36ae959d0cfbec1872b7a3b2
cgxxv <cgsixv@gmail.com>
2024-07-01 12:19:35 +0800

    Update helix config

--- golang.sh
+++ golang.sh
@@ -6,6 +6,7 @@ export GO111MODULE=on
 export GOCACHE=$GOPATH/go-build
 export GOENV=$GOPATH/env
 export GOPROXY=https://goproxy.cn,direct
+export GOFLAGS="-mod=readonly"
 # export GOGC=500
 
 export PATH=$GOPATH/bin:$PATH
