426c3e151105fbd41bc8d0729019a1dccb589caf
cgxxv <cgsixv@gmail.com>
2025-02-25 21:55:14 +0800

    feat: add PNPM_HOME env value

--- export.sh
+++ export.sh
@@ -64,3 +64,6 @@ export PATH=$HOME/fvm/default/bin:$PATH
 export LIBRARY_PATH="/opt/homebrew/lib:$LIBRARY_PATH"
 export C_INCLUDE_PATH="/opt/homebrew/include:$C_INCLUDE_PATH"
 export PKG_CONFIG_PATH="/opt/homebrew/lib/pkgconfig:$PKG_CONFIG_PATH"
+
+# pnpm
+export PNPM_HOME=~/.pnpm
