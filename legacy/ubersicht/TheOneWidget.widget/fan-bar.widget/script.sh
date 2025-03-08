d0bb738d802135d55f75d4289305ee93dcd8d74f
cgxxv <cgsixv@gmail.com>
2025-03-08 11:27:10 +0800

    cleanup

--- legacy/ubersicht/TheOneWidget.widget/fan-bar.widget/script.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-lFan="$(`pwd`/TheOneWidget.widget/fan-bar.widget/osx-cpu-temp -f)"
-cpuTemp="$(`pwd`/TheOneWidget.widget/fan-bar.widget/osx-cpu-temp -C)"
-echo $lFan 
-echo " "$cpuTemp
