8246f5b6ed85ca2dcb4643edaa35b189fc98dde2
cgxxv <cgsixv@gmail.com>
2025-09-13 10:09:31 +0800

    chore: Update environment variables and terminal config files
    - Add CGO flags for include paths.
    - Switch Helix theme to nord_light.
    - Use `brew shellenv` directly.
    - Reorder profile script for cleaner sourcing.
    - Update Wezterm theme to Ubuntu.

--- golang.sh
+++ golang.sh
@@ -1,6 +1,10 @@
 # https://blog.haohtml.com/archives/21778
 #export GODEBUG=allocfreetrace=0
 #export GOROOT_BOOTSTRAP=/usr/local/go
+export CGO_CFLAGS="-O2 -g -I$(brew --prefix)/include"
+export CGO_CXXFLAGS="-O2 -g -I$(brew --prefix)/include"
+export CGO_FFLAGS="-O2 -g -I$(brew --prefix)/include"
+export CGO_LDFLAGS="-O2 -g -L$(brew --prefix)/lib"
 export GOPATH=~/.golang
 export GO111MODULE=on
 export GOCACHE=$GOPATH/go-build
